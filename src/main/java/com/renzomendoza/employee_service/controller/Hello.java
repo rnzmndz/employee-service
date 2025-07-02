e7f44ccd022c5e53d70a30ca9b5a35e209b54501
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-07-02 21:40:14 +0800

    update

---
+++ src/main/java/com/renzomendoza/employee_service/controller/Hello.java
@@ -0,0 +1,19 @@
+package com.renzomendoza.employee_service.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class Hello {
+
+    @Value("${name}")
+    private String name;
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello " + name ;
+    }
+}
