e0f19e04b91a66ebb82fa81300946db16c59481f
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-07-04 16:59:48 +0800

    update

---
+++ src/main/java/com/renzomendoza/employee_service/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.renzomendoza.employee_service.config;
+
+//@Configuration
+//@EnableWebSecurity
+public class SecurityConfig {
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(requests -> requests
+//                        .anyRequest().authenticated()
+//                );
+////                .oauth2ResourceServer(oauth2 -> oauth2
+////                        .jwt()
+////                );
+//        return http.build();
+//    }
+
+}
+
