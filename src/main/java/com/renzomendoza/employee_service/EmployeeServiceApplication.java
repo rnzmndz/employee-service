e91e04359de7da4c73c36f25431b5e5cf9ab4608
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-07-07 21:22:31 +0800

    update

--- src/main/java/com/renzomendoza/employee_service/EmployeeServiceApplication.java
+++ src/main/java/com/renzomendoza/employee_service/EmployeeServiceApplication.java
@@ -6,13 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class EmployeeServiceApplication {
 
-	@GetMapping("/")
-	public String Test(){
-		return "Employee Service Access";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
