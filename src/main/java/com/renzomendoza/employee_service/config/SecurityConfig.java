3a335cd69c73f0912775a3efd7044aaf95d4baf1
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-07-12 14:04:13 +0800

    update

--- src/main/java/com/renzomendoza/employee_service/config/SecurityConfig.java
+++ src/main/java/com/renzomendoza/employee_service/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.renzomendoza.employee_service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,9 +23,26 @@ public class SecurityConfig {
                         .requestMatchers("/public/**").permitAll()
                         .requestMatchers("/webjars/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/api/v1/employees").hasAnyAuthority("ROLE_VIEW_EMPLOYEE_LIST")
                         .anyRequest().authenticated()
                 )
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                        .jwt(jwt -> jwt
+//                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+//                        ))
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthoritiesClaimName("realm_access.roles"); // tells Spring where to find roles
+        converter.setAuthorityPrefix("ROLE_");                   // matches your `hasAuthority("ROLE_X")` check
+
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
+        return jwtConverter;
+    }
+
 }
