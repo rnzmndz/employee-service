adf9d846461f678a4e7aedc7f0f52cbabc70f26b
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-07-21 12:11:00 +0800

    update

--- src/main/java/com/renzomendoza/employee_service/config/SwaggerConfig.java
+++ src/main/java/com/renzomendoza/employee_service/config/SwaggerConfig.java
@@ -1,13 +1,17 @@
 package com.renzomendoza.employee_service.config;
 
+import com.renzomendoza.employee_service.utils.NetworkUtils;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @SecurityScheme(
@@ -20,6 +24,20 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI employeeServiceOpenAPI() {
+        String ip = NetworkUtils.getLocalIpAddress();
+
+        Server localhost = new Server()
+                .url("http://localhost:8080")
+                .description("Localhost");
+
+        Server ipBased = new Server()
+                .url("http://" + ip + ":8080")
+                .description("Current machine IP (" + ip + ")");
+
+        Server docker = new Server()
+                .url("http://host.docker.internal:8080")
+                .description("Docker Host");
+
         return new OpenAPI()
                 .info(new Info().title("Employee Service API")
                         .description("API for managing employee information")
@@ -33,6 +51,7 @@ public class SwaggerConfig {
                                 new io.swagger.v3.oas.models.security.SecurityScheme()
                                         .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT")));
+                                        .bearerFormat("JWT")))
+                .servers(List.of(localhost, ipBased, docker));
     }
 }
